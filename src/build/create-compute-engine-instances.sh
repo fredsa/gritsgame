9b511013cab13ca6e3bfd175d9dd2a0a64217582
Fred Sauer <fredsa@google.com>
2013-04-05 21:31:21 -0700

    updated compute engine script for dynamic zone selection

--- src/build/create-compute-engine-instances.sh
+++ src/build/create-compute-engine-instances.sh
@@ -1,10 +1,46 @@
 #!/bin/bash
+#
+set -ue
+set -v
 
 # constants
 PROJECT=568961353999
-MACHINE_TYPE=n1-standard-1
 STARTUP_SCRIPT="$(dirname $0)/instance-startup-script.sh"
-ZONES=us-central1-a
+
+# select US zone that's not about to go under maintenance 
+ZONES=$(
+  gcutil listzones \
+    --project=google \
+    --filter='status eq UP' \
+    --filter='name eq us-.*' \
+    --format=csv \
+  | cut -d, -f1,5 \
+  | sed 's/\(.*\),\(.*\)/\2,\1/' \
+  | sort -n \
+  | tail -1 \
+  | cut -d, -f2
+)
+
+# select the most recent gcel-* image
+IMAGE=projects/google/global/images/$(
+  gcutil listimages \
+    --project=google \
+    --filter 'description ne .*DEPRECATED.*' \
+    --filter 'name eq gcel-.*' \
+    --format=names \
+  | sort -n \
+  | tail -1)
+
+# select the smallest machinetype
+MACHINE_TYPE=$(
+  gcutil listmachinetypes \
+    --project=google \
+    --filter 'guestCpus eq 1' \
+    --format=names \
+  | sort -n \
+  | head -1
+)
+
 
 $(dirname $0)/check-pairing-key.sh
 
@@ -12,20 +48,21 @@ $(dirname $0)/check-pairing-key.sh
 gcutil getproject --cache_flag_values --project=$PROJECT
 
 # create firewall rules
-gcutil addfirewall http      --allowed="tcp:http"  --description "Incoming http allowed"
-gcutil addfirewall http8080  --allowed="tcp:8080"  --description "Incoming http 8080 allowed"
-gcutil addfirewall httpadmin --allowed="tcp:12345" --description "Incoming http 12345 allowed"
+gcutil addfirewall http      --allowed="tcp:http"  --description "Incoming http allowed"       || true
+gcutil addfirewall http8080  --allowed="tcp:8080"  --description "Incoming http 8080 allowed"  || true
+gcutil addfirewall httpadmin --allowed="tcp:12345" --description "Incoming http 12345 allowed" || true
 
 # list instance options
 gcutil listzones
 gcutil listmachinetypes
 
 # create instance(s)
-for zone in $ZONES;
+for zone in $ZONES
 do
   instance=grits-$zone
   gcutil addinstance $instance \
     --zone=$zone \
+    --image=$IMAGE \
     --machine_type=$MACHINE_TYPE \
     --wait_until_running \
     --metadata_from_file=startup-script:$STARTUP_SCRIPT
