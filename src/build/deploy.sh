374253bda261c94f47420abc5469b897609bed71
Fred Sauer <fredsa@google.com>
2013-04-09 14:19:27 -0700

    updated deploy.sh

--- src/build/deploy.sh
+++ src/build/deploy.sh
@@ -4,7 +4,21 @@ set -ue
 
 APP_DIR="$(dirname $0)/.."
 
-$(dirname $0)/check-pairing-key.sh                                              
+DIRTY_SUFFIX="$( (git diff --exit-code --quiet --cached && git diff --exit-code --quiet) || echo "-dirty" )"
+GIT_VERSION="$( git log -1 --format=%H )$DIRTY_SUFFIX"
+
+if [ $( echo "$*" | egrep -- '-V'\|'--version=' >/dev/null; echo $? ) != 0 ]
+then
+  echo "ERROR: Must explicitly specify either '-V <version>' or '--version <version>'"
+  echo
+  echo "Suggest:"
+  echo
+  echo "  $0 -V $GIT_VERSION"
+  echo
+  exit 1
+fi
+
+$(dirname $0)/check-pairing-key.sh
 
 echo -e "\n*** Rolling back any pending updates (just in case) ***\n"
 appcfg.py --oauth2 $* rollback $APP_DIR
@@ -15,3 +29,16 @@ appcfg.py --oauth2 $* update $APP_DIR
 echo -e "\n*** DEPLOYING BACKENDS ***\n"
 appcfg.py --oauth2 backends $APP_DIR update
 
+
+echo -e "\n*** CHANGE DEFAULT VERSION ***\n"
+cat <<EOD
+#########################################################################
+ ACTION REQUIRED
+
+ Please change the default app version to:
+  $GIT_VERSION
+ using the App Engine admin console:
+   https://appengine.google.com/deployment?&app_id=s~gritsgame
+
+#########################################################################
+EOD
