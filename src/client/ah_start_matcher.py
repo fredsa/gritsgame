5ebed6e469ecfd135c29d330b47887d87adf56d0
Fred Sauer <fredsa@google.com>
2013-04-09 12:26:49 -0700

    cleanup matcher backend startup script

--- src/client/ah_start_matcher.py
+++ src/client/ah_start_matcher.py
@@ -16,32 +16,23 @@ limitations under the License.'''
 from google.appengine.api import apiproxy_stub_map
 from google.appengine.api import backends
 from google.appengine.api import runtime
-from google.appengine.api import users
 
-from google.appengine.ext import webapp
-
-from google.appengine.ext.webapp import template
-
-import cgi
 import datetime
 import logging
-import os
-import urllib
-
-from client import db_api
 
 
-#my_name = '%s.%s (%s)' % (backends.get_backend(), backends.get_instance(), backends.get_url())
-
-logging.info('registering shutdown hook')
+_NAME = '{}.{} {} ({})'.format(backends.get_backend(),
+                               backends.get_instance(),
+                               backends.get_url(),
+                               datetime.datetime.now())
 
 
 def my_shutdown_hook():
-  logging.warning('shutdown hook called')
+  logging.warning('{} SHUTDOWN HOOK CALLED'.format(_NAME))
   apiproxy_stub_map.apiproxy.CancelApiCalls()
   # save_state()
   # May want to raise an exception
 
 # register our shutdown hook, which is not guaranteed to be called
+logging.info('{} REGISTERING SHUTDOWN HOOK'.format(_NAME))
 runtime.set_shutdown_hook(my_shutdown_hook)
-
